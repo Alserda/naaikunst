7ca3cf625f3a513b230c80fe50c5d66a7e5f068b
Jordi Wippert <jor_40bmx@hotmail.com>
2013-11-20 14:35:38 +0100

    Created app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Naaikunst::Application.config.secret_key_base = '56d070b8f025e97c486babd0cda471defa9366c8b3b5a3dc4b95776f14c7a461d7d49b57e75c67a36dfcedef3d31382b0ac16b5269ad2bd3ebb9548490e3e0d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Naaikunst::Application.config.session_store :cookie_store, key: '_naaikunst_session'
